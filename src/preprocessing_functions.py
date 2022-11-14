62fad52cd2e7369be9098476c714576ce3189b6e
Trudes808 <Strudeau12@seaburyhall.orgTrudeau.s@northeastern.edu>
2022-11-14 11:03:06 -0500

    cv2 import bug. Typo

--- src/preprocessing_functions.py
+++ src/preprocessing_functions.py
@@ -9,12 +9,12 @@ def threshold_image(image):
     grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
 
     # global thresholding
-    ret1,th1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+    ret1,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
     # Otsu's thresholding
-    ret2,th2 = cv.threshold(img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+    ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     # Otsu's thresholding after Gaussian filtering
-    blur = cv.GaussianBlur(img,(5,5),0)
-    ret3,th3 = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+    blur = cv2.GaussianBlur(img,(5,5),0)
+    ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
     return th3
 
