c1408fc0708d6a287c7e4f0af4e494709739b3e1
sriram-0311 <114690421+sriram-0311@users.noreply.github.com>
2022-12-11 12:52:09 -0500

    updated nb cv to run on reformatted data

--- src/validate/sign_lang_nb_cv.py
+++ src/validate/sign_lang_nb_cv.py
@@ -5,19 +5,27 @@ from sklearn import naive_bayes
 from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score, GridSearchCV
+# import sklearn mix max scaler
+from sklearn.preprocessing import MinMaxScaler
 
 # reading the csv files using pandas
 train_data = pd.read_csv(
-    "../../input/sign_mnist_train.csv")
+    "preprocessed_asl_train_reformatted.csv")
 test_data = pd.read_csv(
-    "../../input/sign_mnist_test.csv")
+    "preprocessed_asl_train_reformatted.csv")
 
 # Preparing data
-y_train = train_data['label']
-X_train = train_data.drop(columns='label')
+y_train = train_data['LABEL']
+X_train = train_data.drop(columns='LABEL')
 
-y_test = test_data['label']
-X_test = test_data.drop(columns='label')
+# scale input training values to 0 to 1 using min-max scaling
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+
+y_test = test_data['LABEL']
+X_test = test_data.drop(columns='LABEL')
+# scale input test values to 0 to 1 using min-max scaling
+X_test = scaler.fit_transform(X_test)
 
 
 ########################################################################
@@ -26,7 +34,7 @@ X_test = test_data.drop(columns='label')
 # https://www.kaggle.com/code/marioeid/sign-language-mnist-100-accuracy
 ########################################################################
 # GaussianNB model
-start = timeit.default_timer()   
+start = timeit.default_timer()
 model = naive_bayes.GaussianNB()
 model.fit(X_train, y_train)
 
@@ -38,7 +46,7 @@ print("\n")
 scores = cross_val_score(model, X_train, y_train, cv=5)
 stop = timeit.default_timer()
 print("GaussianNB accuracy: %f" % (scores.mean() * 100))
-print("time:", end - start)
+print("time:", stop - start)
 
 # MultinomialNB model
 start = timeit.default_timer()
@@ -50,7 +58,7 @@ y_pred_2 = model.predict(X_test)
 scores = cross_val_score(model, X_train, y_train, cv=5)
 stop = timeit.default_timer()
 print("MultinomialNB accuracy: %f" % (scores.mean() * 100))
-print("time:", end - start)
+print("time:", stop - start)
 
 # ComplementNB model
 start = timeit.default_timer()
@@ -62,7 +70,7 @@ y_pred_3 = model.predict(X_test)
 scores = cross_val_score(model, X_train, y_train, cv=5)
 stop = timeit.default_timer()
 print("ComplementNB accuracy: %f" % (scores.mean() * 100))
-print("time:", end - start)
+print("time:", stop - start)
 
 ################################################################################
 #                           Logistic Regression
@@ -83,16 +91,16 @@ def read_file(filename, hist=False):
                 range(0, 256), fill_value=0).to_numpy()
             pixels[i] = tmp
     else:
-        pixels = df.drop(columns=['label']).values
+        pixels = df.drop(columns=['LABEL']).values
 
-    labels = df.label.values
+    labels = df['LABEL'].values
     pixels = pixels.astype(np.float64)
     return pixels, labels
 
 
 # Read image histogram
 X_train_hist, y_train_hist = read_file(
-    "../../input/sign_mnist_train.csv", hist=True)
+    "preprocessed_asl_train_reformatted.csv", hist=False)
 
 # Train model with histogram features
 start = timeit.default_timer()
@@ -101,4 +109,4 @@ histogram_model = LogisticRegression(
 scores = cross_val_score(histogram_model, X_train_hist, y_train_hist, cv=5)
 stop = timeit.default_timer()
 print("Logistic-regression accuracy: %f" % (scores.mean() * 100))
-print("time:", end - start)
+print("time:", stop - start)
